1d5a7b9052a91b965658ef5d747e6de912225256
olsza-96 <58047088+olsza-96@users.noreply.github.com>
2021-08-22 20:14:30 +0200

    Adding selection of wind turbine with maximized wind production for selected location. Wind base from windpowerlib

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import logging as log
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-import pathlib as p
-import csv
 import seaborn as sns
-from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
+from windpowerlib import ModelChain, WindTurbine
+from windpowerlib import data as wt
 
 log.getLogger().setLevel(log.INFO)
 log.basicConfig(format="%(asctime)s - [%(levelname)s]: %(message)s", datefmt="%H:%M:%S")
@@ -29,7 +29,6 @@ def get_forecast(lat: float, lon:float, API_key:str):
 
     return forecast
 
-
 def clean_data(forecast: list):
     """
     Converts input data to df and gets only relevant data for predicting wind production
@@ -39,8 +38,10 @@ def clean_data(forecast: list):
     df_forecast = pd.DataFrame(forecast)
     df_forecast['dt'] = pd.to_datetime(df_forecast['dt'], unit='s')
     df_forecast = df_forecast.set_index(['dt'], drop=True)
-    df_forecast = df_forecast.drop(columns= [x for x in df_forecast.columns if x not in ['wind_speed', 'wind_deg']])
-    df_forecast = df_forecast.rename(columns= {'wind_speed': 'Forecast_wind_speed'})
+    df_forecast = df_forecast[['wind_speed', 'pressure', 'temp']]
+    df_forecast['pressure'] = df_forecast['pressure'].mul(100) #convert to Pa
+    df_forecast = df_forecast.rename(columns= {'temp': 'temperature'})
+
     return df_forecast
 
 
@@ -54,136 +55,137 @@ def calculate_vertical_wind_distribution(roughness_coefficient:float, height_rot
     :param height_rotor: height of rotor, m
     :param height_forecast: height of anemometer, m
     :param forecast_velocity: dataframe with predicted parameters
-    :return: dataframe with additional column containing wind velocities at rotor height
+    :return: dataframe with all the information for power production modelling
     """
 
     log.debug("Recalculating forecasted wind speed at anemometer height to wind speed at rotor height")
-
-    forecast_velocity['Real_wind_speed'] = forecast_velocity['Forecast_wind_speed']* pow((height_rotor/height_forecast)
+    forecast_velocity['roughness_length'] = roughness_coefficient
+    forecast_velocity['wind_speed_100'] = forecast_velocity['wind_speed']* pow((height_rotor/height_forecast)
                                                                                          ,roughness_coefficient)
+
+    arrays = [['wind_speed', 'pressure', 'temperature', 'roughness_length', 'wind_speed_100'],[10,0,2,0,100]]
+    forecast_velocity.columns = pd.MultiIndex.from_arrays(arrays, names=('variable', 'height'))
+    forecast_velocity = forecast_velocity.rename(columns= {'wind_speed_100': 'wind_speed'})
 
     return forecast_velocity
 
-
-def get_power_curve(file_name: str):
+def get_turbine_library():
     """
-    Read power curve values for chosen type of turbine
-    from https://www.thewindpower.net/turbine_en_33_vestas_v90-3000.php
-    here used Vestas V112/3000
-    :param file_name: file with power curve for chosen turbine
-    :return: dict with power curve
+    Get all the turbines available in the windpowerlib database
+    :return: dataframe with all the turbine names available in the database
     """
-    
-    power_curve_path: p.Path = p.Path.cwd().joinpath(file_name)
+    turbine_database = wt.get_turbine_types(print_out=False)
 
-    with power_curve_path.open(mode="r", encoding="utf-8") as read_file:
-        file_reader = csv.DictReader(read_file)
+    return turbine_database
 
-        dict_ = list(file_reader)[0]
-        power_curve = dict(dict_)
-
-    for k,v in power_curve.items():
-        power_curve[k] = float(v)
-
-    return power_curve
-
-def calculate_wind_production(power_curve: dict, forecast: pd.DataFrame):
-
+def iterate_turbine_library(turbine_database: pd.DataFrame, weather_forecast: pd.DataFrame):
     """
-    Calculate wind production for given turbine and hourly forecast
-    :param power_curve: dict with power for each wind speed
-    :param forecast: input df with forecast data
-    :return: power production in kWh
+    Function iterating over all the turbines available in the database, calculating power output for given forecast
+    :param turbine_database: dataframe with all the turbines available in the database
+    :param weather_forecast: dataframe with hourly weather forecast
+    :return: None
+    """
+    results = []
+    for index, row in turbine_database.iterrows():
+        result_dict = {}
+        turbine = initialize_wind_turbine(row['turbine_type'])
+        results_turbine = calculate_power_output(turbine, weather_forecast)
+        result_dict['turbine_type'] = row['turbine_type']
+        result_dict['power_output_hourly'] = results_turbine.power_output
+        result_dict['energy_produced_kWh'] = round(results_turbine.power_output.sum(),2)
+        results.append(result_dict)
+
+    max_idx = find_maximum_power(results)
+    plot_power_production(results[max_idx], weather_forecast)
+
+def initialize_wind_turbine(turbine_type):
+    """
+    Initialize parameters of turbine from database with given turbine type
+    :param turbine_type: name of turbine
+    :return: turbine parameters
+    """
+    turbine_args = {
+        'turbine_type': turbine_type,  # turbine type as in oedb turbine library
+        'hub_height': 120
+    }
+    turbine = WindTurbine(**turbine_args)
+
+    return turbine
+
+def calculate_power_output(turbine, weather_forecast):
+    """
+    Calculate predicted horly power output for selected wind turbine type
+    :param turbine: turbine type from the database
+    :param weather_forecast: predicted hourly weather conditions from the weather API
+    :return: hourly power output from selected turbine, in KW
     """
 
-    log.debug("Calculating forecasted hourly energy production for the following 48 hrs")
-    power_production = []
-    for wind_speed in forecast['Real_wind_speed']:
-        power_production.append(power_curve[str(round(wind_speed*2)/2)])
+    modelchain_data = {
+        'wind_speed_model': 'logarithmic',      # 'logarithmic' (default),'hellman' or 'interpolation_extrapolation'
+        'density_model': 'barometric',           # 'barometric' (default), 'ideal_gas' or 'interpolation_extrapolation'
+        'temperature_model': 'linear_gradient', # 'linear_gradient' (def.) or 'interpolation_extrapolation'
+        'power_output_model':
+            'power_curve',          # 'power_curve' (default) or 'power_coefficient_curve'
+        'density_correction': False,             # False (default) or True
+        'obstacle_height': 0,                   # default: 0
+        'hellman_exp': None}                    # None (default) or None
 
-    forecast['Power_production'] = power_production
+    model_turbine = ModelChain(turbine, **modelchain_data).run_model(weather_forecast)
+    turbine.power_output = model_turbine.power_output/1000 #return hourly power output in kW
 
-    return forecast
+    #print(f"Calculated power output for {turbine.turbine_type} is {round(turbine.power_output.sum(),2)} kWh")
 
+    return turbine
 
-def wind_orientation(forecast: pd.DataFrame):
+def find_maximum_power(results: list):
     """
-    Adding info on wind direction for each wind speed 
-    :param forecast: input df with forecast data
-    :return: info on direction of wind blowing
+    Function returning parameters of the turbine giving the maximum output in selected conditions
+    :param results: list with all predicted power outputs for turbines in the database
+    :return: None
     """
-    #calculating and printing wind orientations and energy production for all the directions
-    northern_wind = forecast.query("(wind_deg>=0 & wind_deg<=45) | \
-                                   (wind_deg >= 315 & wind_deg <=360)").index
-    eastern_wind = forecast.query("wind_deg>45 & wind_deg<=135").index
-    southern_wind = forecast.query("wind_deg>135 & wind_deg<=225").index
-    western_wind = forecast.query("wind_deg>225 & wind_deg<315").index
+    results = [{k: v for k, v in d.items() if k != 'power_output_hourly'} for d in results]
+    df_results = pd.DataFrame(results)
 
-    forecast.loc[northern_wind, 'wind_orientation'] = "northern"
-    forecast.loc[eastern_wind, 'wind_orientation'] = "eastern"
-    forecast.loc[southern_wind, 'wind_orientation'] = "southern"
-    forecast.loc[western_wind, 'wind_orientation'] = "western"
+    max_power_idx = df_results['energy_produced_kWh'].idxmax()
+    print(f"The turbine with the greatest power output is {df_results['turbine_type'].iloc[max_power_idx]} "
+          f"and would produce {round(df_results['energy_produced_kWh'].iloc[max_power_idx]/1000,2)} MWh of electric energy"
+          f" in the upcoming 4 days")
 
-    return forecast
+    return max_power_idx
 
-def statistics(forecast: pd.DataFrame, cut_in_speed: float, cut_off_speed: float):
-
+def plot_power_production(wind_production: dict, forecast: pd.DataFrame):
     """
-    Print statistics of predicted power production
-    :param forecast: df with info on forecasted production
-    :param cut_in_speed: minimum wind speed at which turbine is able to generate electricity
-    :param cut_off_speed: maximum wind speed at which turbine can work
-    """
-    no_energy_produced = len(forecast.query(f"Real_wind_speed < {cut_in_speed}  | \
-                                   Real_wind_speed > {cut_off_speed} ").index)
-
-    log.info(f"Forecast was computed for the following dates: {forecast.index.min()} - {forecast.index.max()}")
-    log.info(f"The forecasted power  = {round(forecast['Power_production'].sum()/1000,2)} MWh")
-    log.info(f"Wind parameters for the following dates are min = {round(forecast['Real_wind_speed'].min(),2)} m/s"
-             f"max = {round(forecast['Real_wind_speed'].max(),2)} m/s"
-             f"mean = {round(forecast['Real_wind_speed'].mean(),2)} m/s")
-    log.info(f"Number of hours with no energy produced {no_energy_produced}")
-
-def plotting(forecast: pd.DataFrame):
-    """
-    Plot hourly energy production for the following 48 hrs
-    and wind speed histogram by wind direction
-    :param forecast: df with info on forecast
+    Plot results of wind forecast and predicted power output
+    :param wind_production: predicted hourly power output
+    :param forecast: wind prediction
+    :return: None
     """
     sns.set_theme(style="ticks")
     fig, ax = plt.subplots(2)
 
-    df_agg = forecast.loc[:, ['Real_wind_speed', 'wind_orientation']].groupby('wind_orientation')
-    vals = [df['Real_wind_speed'].values.tolist() for i, df in df_agg]
+    hours = mdates.HourLocator(interval = 4)
+    h_fmt = mdates.DateFormatter('%d/%m %H:%M')
 
-    # Draw
-    colors = [plt.cm.Set1(i/float(len(vals)-1)) for i in range(len(vals))]
-    n, bins, patches = ax[0].hist(vals, 60, stacked=True, density=False, color=colors[:len(vals)])
+    ax[0].plot(forecast[('wind_speed',100)].index, forecast[('wind_speed',100)].values, color = 'red')
+    ax[0].set_title(f"Predicted weather conditions on the height of 100 m")
+    ax[0].set_xlabel('Date')
+    ax[0].set_ylabel('Wind speed, m/s')
+    ax[0].xaxis.set_major_locator(hours)
+    ax[0].xaxis.set_major_formatter(h_fmt)
 
-    # Decoration
-    ax[0].legend({group:col for group, col in zip(np.unique(forecast['wind_orientation']).tolist(), colors[:len(vals)])})
-    ax[0].set_title(f"Histogram of wind speed by wind direction")
-    ax[0].set_ylabel("Number of hours ")
-    ax[0].set_xticks(ticks=bins[::3])
-    ax[0].set_xticklabels([round(b,1) for b in bins[::3]])
-
-    ax[1].plot(forecast.index, forecast['Power_production']/1000, c='lightcoral')
-    ax[1].set_title(f"Forecasted hourly energy production")
-    ax[1].set_xlabel("Date")
-    ax[1].set_ylabel("Energy production, MWh")
-    ax[1].xaxis.set_major_formatter(DateFormatter('%d %b %H'))
-    plt.setp(ax[1].get_xticklabels(), rotation=30, horizontalalignment='right')
-
-
+    ax[1].plot(wind_production['power_output_hourly'].index, wind_production['power_output_hourly'], color = 'green')
+    ax[1].set_title(f"Predicted power production for turbine {wind_production['turbine_type']}")
+    ax[1].set_xlabel('Date')
+    ax[1].set_ylabel('Power output, kW')
+    ax[1].xaxis.set_major_locator(hours)
+    ax[1].xaxis.set_major_formatter(h_fmt)
+    fig.autofmt_xdate(rotation=45)
     plt.show()
 
 if __name__ == "__main__":
     forecast_list  = get_forecast(54.335472056066855, 16.564964098411, "ccabb6fe85ca3bac888578fe83955ed2")
     forecast_48hrs = clean_data(forecast_list)
-    forecast_48hrs = calculate_vertical_wind_distribution(roughness_coefficient= 0.22, height_rotor=150.,
+    forecast_48hrs = calculate_vertical_wind_distribution(roughness_coefficient= 0.22, height_rotor=100.,
                                                         height_forecast=10.,forecast_velocity=forecast_48hrs)
-    power_curve = get_power_curve('power_curve.csv')
-    forecast_48hrs = calculate_wind_production(power_curve, forecast_48hrs)
-    forecast_48hrs = wind_orientation(forecast_48hrs)
-    statistics(forecast_48hrs, cut_in_speed=3.5, cut_off_speed=25.)
-    plotting(forecast_48hrs)
+    turbine_database= get_turbine_library()
+    iterate_turbine_library(turbine_database,forecast_48hrs)
